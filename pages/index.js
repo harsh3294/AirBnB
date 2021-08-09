7502184d0b4b1560029420cc262642de52107622
harsh3294 <harshc3294@gmail.com>
2021-08-09 17:40:41 +0530

    created live anywhere component and added plugin

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,9 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
 import SmallCard from "../components/SmallCard";
-export default function Home({ exploreData }) {
-  console.table(exploreData);
+import MediumCard from "../components/MediumCard";
+export default function Home({ exploreData, cardsData }) {
+  console.table(cardsData);
   return (
     <div>
       <Head>
@@ -15,14 +16,22 @@ export default function Home({ exploreData }) {
       <main className="max-w-7xl mx-auto px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
-          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData?.map((item) => (
               <SmallCard
-                key={item.img}
-                img={item.img}
-                location={item.location}
-                distance={item.distance}
+                key={item?.img}
+                img={item?.img}
+                location={item?.location}
+                distance={item?.distance}
               />
+            ))}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-4 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map((item) => (
+              <MediumCard key={item?.img} img={item?.img} title={item?.title} />
             ))}
           </div>
         </section>
@@ -35,9 +44,13 @@ export async function getStaticProps() {
   const exploreData = await fetch("https://links.papareact.com/pyp").then(
     (res) => res.json()
   );
+  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
   return {
     props: {
       exploreData,
+      cardsData,
     },
   };
 }
