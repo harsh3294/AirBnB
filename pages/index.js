87f6f59710202458e1e371e41bc80553219f1340
harsh3294 <harshc3294@gmail.com>
2021-08-09 17:11:00 +0530

    completed explore nearby places

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
-export default function Home() {
+import SmallCard from "../components/SmallCard";
+export default function Home({ exploreData }) {
+  console.table(exploreData);
   return (
     <div>
       <Head>
@@ -10,6 +12,32 @@ export default function Home() {
       </Head>
       <Header />
       <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map((item) => (
+              <SmallCard
+                key={item.img}
+                img={item.img}
+                location={item.location}
+                distance={item.distance}
+              />
+            ))}
+          </div>
+        </section>
+      </main>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://links.papareact.com/pyp").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      exploreData,
+    },
+  };
 }
